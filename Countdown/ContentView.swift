441861c131f384eb3f4934441ac7f5c1439bc409
Cameron <caslash@bsu.edu>
2022-05-26 19:10:05 -0400

    Initial Commit

---
+++ Countdown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Countdown
+//
+//  Created by Cameron Slash on 26/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
