76a2a692cde47f7dc0f6d5be3e6bbb58026e55f0
Cameron <caslash@bsu.edu>
2022-06-26 18:58:38 -0400

    Refactored persistent container

--- CounterDown/CoreData/DataController.swift
+++ CounterDown/CoreData/DataController.swift
@@ -15,16 +15,18 @@ class DataController: ObservableObject {
         DataController()
     }()
     
-    let container = NSPersistentContainer(name: "CounterDownModel")
-    
-    init() {
+    let container: NSPersistentContainer = {
+       var container = NSPersistentContainer(name: "CounterDownModel")
+        
         container.loadPersistentStores { description, error in
             if let error = error {
                 print("Core Data failed to load: \(error.localizedDescription)")
                 return
             }
             
-            self.container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+            container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
         }
-    }
+        
+        return container
+    }()
 }
