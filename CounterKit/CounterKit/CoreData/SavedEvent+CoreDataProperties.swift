5afd4be091eec8e91fb4d8f438af0c7a2e059071
Cameron <caslash@bsu.edu>
2022-07-04 22:42:29 -0400

    Fixed CounterKit to allow Mac Apps to import

--- CounterKit/CounterKit/CoreData/SavedEvent+CoreDataProperties.swift
+++ CounterKit/CounterKit/CoreData/SavedEvent+CoreDataProperties.swift
@@ -10,9 +10,8 @@ import CoreData
 import Foundation
 import SwiftUI
 
-
+#if canImport(UIKit)
 extension SavedEvent {
-
     @nonobjc public class func fetchRequest() -> NSFetchRequest<SavedEvent> {
         return NSFetchRequest<SavedEvent>(entityName: "SavedEvent")
     }
@@ -62,3 +61,55 @@ extension SavedEvent {
 extension SavedEvent : Identifiable {
 
 }
+#elseif canImport(AppKit)
+extension SavedEvent {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<SavedEvent> {
+        return NSFetchRequest<SavedEvent>(entityName: "SavedEvent")
+    }
+
+    @NSManaged public var id: UUID?
+    @NSManaged public var name: String?
+    @NSManaged public var due: Date?
+    @NSManaged public var colorHex: String?
+    @NSManaged public var components: Data?
+
+    public var wrappedId: UUID { id ?? UUID() }
+    private var wrappedName: String { name ?? "Unknown Event Name" }
+    private var wrappedDue: Date { due ?? Date() }
+    private var wrappedColor: Color {
+        if let colorHex = colorHex {
+            return Color(NSColor(hexString: colorHex))
+        } else {
+            return Color.red
+        }
+    }
+    private var wrappedComponents: Set<Calendar.Component> {
+        if let saved_components = components {
+            if let decoded = try? JSONDecoder().decode(Set<Calendar.Component>.self, from: saved_components) {
+                return decoded
+            }
+        }
+        return [.day, .hour, .minute, .second]
+    }
+    
+    public var wrappedEvent: Event {
+        return Event(id: wrappedId, name: wrappedName, due: wrappedDue, color: wrappedColor, components: wrappedComponents)
+    }
+    
+    public static var previewSavedEvent: SavedEvent {
+        let nyd = PreviewEvents.nyd
+        let viewContext = DataController.preview.container.viewContext
+        let event = SavedEvent(context: viewContext)
+        event.id = nyd.id
+        event.name = nyd.name
+        event.due = nyd.due
+        event.colorHex = NSColor(nyd.color).toHexString()
+        event.components = try? JSONEncoder().encode(nyd.components)
+        return event
+    }
+}
+
+extension SavedEvent : Identifiable {
+
+}
+#endif
