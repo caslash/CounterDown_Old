4c681dae19a55e2b92f86e91ca331e6d7548f414
Cameron <caslash@bsu.edu>
2022-07-13 22:07:41 -0400

    Added visual indicator of event recurrence

--- CounterDown/CounterDown/Views/Components/GradientEventView.swift
+++ CounterDown/CounterDown/Views/Components/GradientEventView.swift
@@ -17,17 +17,11 @@ struct GradientEventView: View {
     @ObservedObject var viewmodel: EventViewModel
     @State private var showingEditSheet = false
     var body: some View {
-        VStack {
-            ZStack {
-                Text(self.viewmodel.event.eventName)
-                    .lineLimit(2)
-                    .multilineTextAlignment(.center)
-                    .truncationMode(.tail)
-                    .font(.title.weight(.black))
+        ZStack {
+            HStack {
+                Spacer()
                 
-                HStack {
-                    Spacer()
-                    
+                VStack {
                     Menu {
                         Button {
                             self.showingEditSheet = true
@@ -45,83 +39,110 @@ struct GradientEventView: View {
                         Image(systemName: .ellipsisCircle)
                             .font(.title3)
                     }
+                    
+                    Spacer()
                 }
             }
             
-            HStack(spacing: 5) {
-                if self.viewmodel.event.eventComponents.contains(.year) {
+            
+            if self.viewmodel.event.isRecurring {
+                HStack {
+                    Spacer()
+                    
                     VStack {
-                        Text(self.viewmodel.yearsLeft())
+                        Spacer()
+                        
+                        Text(self.viewmodel.event.eventRecurrenceInterval.displayName)
+                            .font(.footnote.weight(.bold))
+                            .padding(3)
+                            .background(.ultraThinMaterial)
+                            .cornerRadius(5)
+                    }
+                }
+            }
+            
+            VStack {
+                Text(self.viewmodel.event.eventName)
+                    .lineLimit(2)
+                    .multilineTextAlignment(.center)
+                    .truncationMode(.tail)
+                    .font(.title.weight(.black))
+            
+                HStack(spacing: 5) {
+                    if self.viewmodel.event.eventComponents.contains(.year) {
+                        VStack {
+                            Text(self.viewmodel.yearsLeft())
+                                .font(.title3.weight(.black))
+                            
+                            Text("YR")
+                                .font(.subheadline.weight(.semibold))
+                        }
+                        .frame(width: 50, height: 50)
+                        .background(.ultraThinMaterial)
+                        .cornerRadius(10)
+                    }
+                    
+                    if self.viewmodel.event.eventComponents.contains(.month) {
+                        VStack {
+                            Text(self.viewmodel.monthsLeft())
+                                .font(.title3.weight(.black))
+                            
+                            Text("MTH")
+                                .font(.subheadline.weight(.semibold))
+                        }
+                        .frame(width: 50, height: 50)
+                        .background(.ultraThinMaterial)
+                        .cornerRadius(10)
+                    }
+                    
+                    VStack {
+                        Text(self.viewmodel.daysLeft())
                             .font(.title3.weight(.black))
                         
-                        Text("YR")
+                        Text("DAY")
+                            .font(.subheadline.weight(.semibold))
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(10)
+                    
+                    VStack {
+                        Text(self.viewmodel.hoursLeft())
+                            .font(.title3.weight(.black))
+                        
+                        Text("HR")
+                            .font(.subheadline.weight(.semibold))
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(10)
+                    
+                    VStack {
+                        Text(self.viewmodel.minutesLeft())
+                            .font(.title3.weight(.black))
+                        
+                        Text("MIN")
+                            .font(.subheadline.weight(.semibold))
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(10)
+                    
+                    VStack {
+                        Text(self.viewmodel.secondsLeft())
+                            .font(.title3.weight(.black))
+                        
+                        Text("SEC")
                             .font(.subheadline.weight(.semibold))
                     }
                     .frame(width: 50, height: 50)
                     .background(.ultraThinMaterial)
                     .cornerRadius(10)
                 }
-                
-                if self.viewmodel.event.eventComponents.contains(.month) {
-                    VStack {
-                        Text(self.viewmodel.monthsLeft())
-                            .font(.title3.weight(.black))
-                        
-                        Text("MTH")
-                            .font(.subheadline.weight(.semibold))
-                    }
-                    .frame(width: 50, height: 50)
-                    .background(.ultraThinMaterial)
-                    .cornerRadius(10)
-                }
-                
-                VStack {
-                    Text(self.viewmodel.daysLeft())
-                        .font(.title3.weight(.black))
-                    
-                    Text("DAY")
-                        .font(.subheadline.weight(.semibold))
-                }
-                .frame(width: 50, height: 50)
-                .background(.ultraThinMaterial)
-                .cornerRadius(10)
-                
-                VStack {
-                    Text(self.viewmodel.hoursLeft())
-                        .font(.title3.weight(.black))
-                    
-                    Text("HR")
-                        .font(.subheadline.weight(.semibold))
-                }
-                .frame(width: 50, height: 50)
-                .background(.ultraThinMaterial)
-                .cornerRadius(10)
-                
-                VStack {
-                    Text(self.viewmodel.minutesLeft())
-                        .font(.title3.weight(.black))
-                    
-                    Text("MIN")
-                        .font(.subheadline.weight(.semibold))
-                }
-                .frame(width: 50, height: 50)
-                .background(.ultraThinMaterial)
-                .cornerRadius(10)
-                
-                VStack {
-                    Text(self.viewmodel.secondsLeft())
-                        .font(.title3.weight(.black))
-                    
-                    Text("SEC")
-                        .font(.subheadline.weight(.semibold))
-                }
-                .frame(width: 50, height: 50)
-                .background(.ultraThinMaterial)
-                .cornerRadius(10)
             }
         }
         .foregroundColor(UIColor(self.viewmodel.event.eventColor).isLight() ? .black : .white)
-        .frame(width: (UIScreen.main.bounds.width / 4) * 3)
+        .frame(width: (UIScreen.main.bounds.width / 5) * 4, height: UIScreen.main.bounds.height / 7)
         .padding()
         .background(self.viewmodel.event.eventColor.gradient)
         .cornerRadius(20)
